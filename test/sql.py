19582889c7d7cf6c9d95dd06dae6aa9e5641c4f0
Sheldon McLeod <samgt13@gmail.com>
2020-03-10 18:42:48 -0400

    updates

--- test/sql.py
+++ test/sql.py
@@ -1,22 +1,30 @@
 import pymysql
+import sqlconfig
+import pandas as pd
+import sqlalchemy
+
+
+
+passwd = sqlconfig.passwd
+user = sqlconfig.user
 
 
 
 
 
 
-connection = pymysql.connect(host='34.67.192.90',
-      user='root',password='CEA299792458',
-      db='sample')
+connection = pymysql.connect(host=34.67.192.90,
+      user=root,password=CEA299792458,
+      db=sample)
 
 c = connection.cursor()
 
 
-c.execute('CREATE TABLE CARS (Brand text, Price number)')
+c.execute(CREATE TABLE CARS (Brand text, Price number))
 conn.commit()
 
-Cars = {'Brand': ['Honda Civic','Toyota Corolla','Ford Focus','Audi A4'],
-        'Price': [22000,25000,27000,35000]
+Cars = {Brand: [Honda Civic,Toyota Corolla,Ford Focus,Audi A4],
+        Price: [22000,25000,27000,35000]
         }
 
-df = DataFrame(Cars, columns= ['Brand', 'Price'])
+df = DataFrame(Cars, columns= [Brand, Price])
