7fb376e2450d7af79ec8baaa2d965f6871d0985d
Sheldon McLeod <samgt13@gmail.com>
2020-03-20 13:25:40 -0400

    updates

--- test/sql.py
+++ test/sql.py
@@ -28,3 +28,38 @@ Cars = {Brand: [Honda Civic,Toyota Corolla,Ford Focus,Audi A4],
         }
 
 df = DataFrame(Cars, columns= [Brand, Price])
+
+
+
+
+import pymysql
+import sqlconfig
+import pandas as pd
+import sqlalchemy
+
+
+
+passwd = 'CEA299792458'
+user = 'postgres'
+DB = 'newlab'
+
+
+engine = sqlalchemy.create_engine('mysql+pymysql://'+user+':'+passwd+'@localhost/'+DB)
+
+
+base_path = os.path.dirname(os.path.dirname(os.path.abspath(os. getcwd())))
+'/home/sheldon'
+
+start_path = os.path.join(base_path,"dashboard","Plotly_dash","CSV", "1Processed","1234")
+start_path
+
+
+
+fn = 'protoCBAS-*'
+path = sorted(glob.glob(os.path.join(start_path, fn)))
+path
+
+upload.to_sql('pet',engine,
+    if_exists='append',
+    index=False
+)
